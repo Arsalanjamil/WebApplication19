aab3042485f3413496128b61d145dddc07c9b074
Arsalan <Arsalan@DESKTOP-AV9PQM6>
2021-07-18 10:36:34 +0500

    Add project files.

---
+++ WebApplication19/Controllers/DepartmentController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication19.Models;
+
+namespace WebApplication19.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        //private readonly AppDbContext context;
+
+        //public DepartmentController(AppDbContext context)
+        //{
+        //    this.context = context;
+        //}
+
+        //[HttpPost]
+        ////[Route("Department/Save")]
+        //public IActionResult Save([FromBody] Department department)
+        //{
+        //    if (department.DepartmentID == 0)
+        //    {
+        //        context.Departments.Add(department);
+        //    }
+        //    else
+        //    {
+        //        var dep = context.Departments.Attach(department);
+        //        dep.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        //    }
+        //    context.SaveChanges();
+
+        //    //return department;
+        //    return Json(department);
+        //}
+
+        //[HttpDelete]
+        ////[Route("Department/delete/{id}")]
+        //public IActionResult delete(int id)
+        //{
+        //    Department department = context.Departments.Find(id);
+        //    if (department != null)
+        //    {
+        //        context.Departments.Remove(department);
+        //        context.SaveChanges();
+        //    }
+        //    return Json(department);
+        //}
+
+
+        //public IActionResult GetAllDepartment(int id)
+        //{
+        //    if (id == 0)
+        //    {
+        //        return Json(context.Departments);
+        //    }
+        //    return Json(context.Departments.Find(id));
+        //}
+
+    }
+}
---
+++ WebApplication19/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication19.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        [MaxLength(50)]
+        public string DepartmentName { get; set; }
+        [MaxLength(300)]
+        public string Description { get; set; }
+    }
+}
+
